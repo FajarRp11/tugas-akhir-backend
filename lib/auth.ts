fa87ecc58dec68d57ccbbebd231b3d12367a0675
Kurnia Fajar Rahyudi Putra <142131612+FajarRp11@users.noreply.github.com>
2026-04-16 15:38:07 +0700

    Change token expiration from 1 minute to 1 day

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export function generateToken(payload: any) {
-  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: '1m' })
+  return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: '1d' })
 }
 
 export function verifyToken(request: Request) {
